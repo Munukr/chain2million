50f7694e900b58b6e9451b21771c63853dbd4d34
Munukr <0543253444d@gmail.com>
2025-06-06 19:42:59 +0300

    feat: add temporary console.log in /api/user/:id and /api/admin/users routes

--- api/index.js
+++ api/index.js
@@ -26,6 +26,16 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 app.get('/admin.html', (req, res) => res.sendFile(path.join(__dirname, '../public/admin.html')));
 
+app.get('/api/user/:id', async (req, res) => {
+  console.log('GET /api/user/:id - запрос получен, id:', req.params.id);
+  // ... existing code ...
+});
+
+app.get('/api/admin/users', async (req, res) => {
+  console.log('GET /api/admin/users - запрос получен');
+  // ... existing code ...
+});
+
 app.use((req, res) => res.status(404).send('Not found'));
 
 module.exports = app; 
